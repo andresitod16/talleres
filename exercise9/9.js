b20000d739824bb7cf4f073be5698017ebf7d493
andres <roandres730@gmail.com>
2026-04-30 20:35:54 -0500

    coloresarray

---
+++ exercise9/9.js
@@ -0,0 +1,8 @@
+// Pide al usuario 3 colores (usando prompt)
+// y guárdalos en un array (colores). Luego,
+// muéstralos con console.log.
+
+let colores = (prompt("ingresa tus colores"));
+let array = [];
+array.push(colores);
+console.log(colores);
