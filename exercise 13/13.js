06a794bbe3e70e37c109ca3982709bfdb26cacd2
andres <roandres730@gmail.com>
2026-05-03 15:46:33 -0500

    profesion

---
+++ exercise 13/13.js
@@ -0,0 +1,11 @@
+// 13. Agrega una nueva propiedad
+// (profesion) al objeto persona y
+// muéstrala.
+let persona = {
+    nombre: "andres", 
+    edad: 19,
+    ciudad : "bogota",
+    profesion : "ingeniero"
+};
+
+console.log (persona.profesion)
