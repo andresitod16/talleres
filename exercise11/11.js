a9e96ef00a05a9ebdd24b431be13f1f954bf73ca
andres <roandres730@gmail.com>
2026-05-03 15:41:40 -0500

    ciudad

---
+++ exercise11/11.js
@@ -0,0 +1,13 @@
+// Crea un objeto (persona) con
+// propiedades: nombre, edad, ciudad.
+// Muestra cada propiedad usando notación
+// de punto (persona.nombre) y notación
+// de corchetes (persona['edad']).
+
+let persona = {
+    nombre: "andres", 
+    edad: 19,
+    ciudad : "bogota"
+};
+console.log(persona.nombre)
+console.log(persona["nombre"])
---
+++ exercise12/12.js
@@ -0,0 +1,10 @@
+// Modifica una propiedad: Cambia la
+// ciudad de persona y muestra el objeto
+// actualizado.
+let persona = {
+    nombre: "andres", 
+    edad: 19,
+    ciudad : "bogota"
+};
+persona.ciudad ="ibague"
+console.log(persona);
