573d8e4e2b351f080829373933793bd4849afa8a
andres <roandres730@gmail.com>
2026-05-03 15:53:31 -0500

    punto14

---
+++ execersi14/14.js
@@ -0,0 +1,12 @@
+// Pide al usuario ingresar (nombre, edad)
+// usando el prompt() y crea un objeto
+// (usuario) con ellos.
+let nombre = prompt("ingresa tu nombre");
+let edad = prompt("ingresar edad") ;
+
+let usuario = { 
+    nombre : nombre,
+    edad : edad
+
+}
+console.log (usuario)
