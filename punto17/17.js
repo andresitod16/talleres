5261da025cebbcd2e88cec7f7ef396ce63233e5d
andres <roandres730@gmail.com>
2026-05-04 18:46:32 -0500

    17

---
+++ punto17/17.js
@@ -0,0 +1,11 @@
+// Verifica si un número ingresado por el
+// usuario es positivo, negativo o cero.
+let num = (prompt("ingresa un numero"));
+if (num>0) {
+    console.log ("el numero " , num ,"es positivo")
+}else if (num<0) {
+    console.log ("el numero ", num , "es negativo");
+
+}else if (num==0) {
+    console.log("el numero" , num , "es cero")
+}
